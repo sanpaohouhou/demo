f0507830703a7652453cd3f4a9a7adb0e79ca0f3
qiaowannet <tomie06@163.com>
2020-03-27 17:01:02 +0800

    添加手写aoplog

---
+++ src/main/java/com/example/demo/test/AopLog.java
@@ -0,0 +1,16 @@
+package com.example.demo.test;
+
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class AopLog {
+
+
+
+    // aop 编程里面有几个通知： 前置通知 后置通知 运行通知 异常通知 环绕通知
+}
